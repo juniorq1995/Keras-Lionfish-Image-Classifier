fda881e7257078719bfa35ead4faca5fb86294c5
juniorq1995 <juniorq1995@users.noreply.github.com>
2018-03-02 11:30:44 -0600

    Update Classifier.py

--- first_run/Classifier.py
+++ first_run/Classifier.py
@@ -6,32 +6,27 @@ https://www.kaggle.com/c/dogs-vs-cats/data
 In our setup, we:
 - created a data/ folder
 - created train/ and validation/ subfolders inside data/
-- created cats/ and dogs/ subfolders inside train/ and validation/
-- put the cat pictures index 0-999 in data/train/cats
-- put the cat pictures index 1000-1400 in data/validation/cats
-- put the dogs pictures index 12500-13499 in data/train/dogs
-- put the dog pictures index 13500-13900 in data/validation/dogs
 So that we have 1000 training examples for each class, and 400 validation examples for each class.
 In summary, this is our directory structure:
 ```
 data/
     train/
-        dogs/
-            dog001.jpg
-            dog002.jpg
+        lionfish/
+            lionfish.1.jpg
+            lionfish.2.jpg
             ...
-        cats/
-            cat001.jpg
-            cat002.jpg
+        notLionfish/
+            notLionfish.1.jpg
+            notLionfish.2.jpg
             ...
     validation/
-        dogs/
-            dog001.jpg
-            dog002.jpg
+        lionfish/
+            lionfish.1.jpg
+            lionfish.2.jpg
             ...
-        cats/
-            cat001.jpg
-            cat002.jpg
+        notLionfish/
+            notLionfish.1.jpg
+            notLionfish.2.jpg
             ...
 ```
 '''
